a804ac27be071903c2d367affe6ad433c1876a99
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-03-27 14:30:02 +0200

    [ADD] Creating TextFieldIcon component + Adding an glowing animation on when textfield is selected

--- SwiftUI Advanced/ContentView.swift
+++ SwiftUI Advanced/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var editingEmailTextfield: Bool = false
+    @State private var editingPasswordTextfield: Bool = false
     
     var body: some View {
         ZStack {
@@ -31,10 +33,12 @@ struct ContentView: View {
                         .foregroundColor(.white.opacity(0.7))
                     
                     HStack(spacing: 12) {
-                        Image(systemName: "envelope.open.fill")
-                            .foregroundColor(.white)
+                        TextfieldIcon(iconName: "envelope.open.fill", currentlyEditing: $editingEmailTextfield)
                         
-                        TextField("Email", text: $email)
+                        TextField("Email", text: $email) { isEditing in
+                            editingEmailTextfield = isEditing
+                            editingPasswordTextfield = false
+                        }
                             .preferredColorScheme(.dark)
                             .foregroundColor(.white.opacity(0.7))
                             .textInputAutocapitalization(.none)
@@ -53,8 +57,7 @@ struct ContentView: View {
                     )
                     
                     HStack(spacing: 12) {
-                        Image(systemName: "key.fill")
-                            .foregroundColor(.white)
+                        TextfieldIcon(iconName: "key.fill", currentlyEditing: $editingPasswordTextfield)
                         
                         SecureField("Password", text: $password)
                             .preferredColorScheme(.dark)
@@ -73,6 +76,10 @@ struct ContentView: View {
                             .cornerRadius(16)
                             .opacity(0.8)
                     )
+                    .onTapGesture {
+                        editingPasswordTextfield = true
+                        editingEmailTextfield = false
+                    }
                     
                     GradientButton()
                     
@@ -114,73 +121,6 @@ struct ContentView: View {
         }
     }
 }
-
-extension View {
-    public func gradientForeground(colors: [Color]) -> some View {
-        self
-            .overlay(
-                LinearGradient(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
-            )
-            .mask(self)
-    }
-}
-
-struct GradientText: View {
-    
-    var text: String = "Text here"
-    
-    var body: some View {
-        Text(text)
-            .gradientForeground(colors: [Color("pink-gradient-1"), Color("pink-gradient-2")])
-    }
-}
-
-struct GradientButton: View {
-    
-    var gradient1: [Color] = [
-        Color.init(red: 101/255, green: 134/255, blue: 1),
-        Color.init(red: 1, green: 64/255, blue: 80/255),
-        Color.init(red: 109/255, green: 1, blue: 185/255),
-        Color.init(red: 39/255, green: 232/255, blue: 1)
-    ]
-    
-    var body: some View {
-        Button(action: {
-            print("Sign up")
-        }, label: {
-            GeometryReader() { geometry in
-                ZStack {
-                    AngularGradient(gradient: Gradient(colors: gradient1), center: .center, angle: .degrees(0))
-                        .blendMode(.overlay)
-                        .blur(radius: 8)
-                        .mask(
-                            RoundedRectangle(cornerRadius: 16)
-                                .frame(height: 50)
-                                .frame(maxWidth: geometry.size.width - 16)
-                                .blur(radius: 8)
-                        )
-                    GradientText(text: "Sign up")
-                        .font(.headline)
-                        .frame(width: geometry.size.width - 16)
-                        .frame(height: 50)
-                        .background(
-                            Color("tertiaryBackground")
-                                .opacity(0.9)
-                        )
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 16)
-                                .stroke(.white, lineWidth: 1)
-                                .blendMode(.normal)
-                                .opacity(0.7)
-                        )
-                        .cornerRadius(16)
-                }
-            }
-            .frame(height: 50)
-        })
-    }
-}
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ SwiftUI Advanced/Views/GradientText.swift
@@ -0,0 +1,28 @@
+//
+//  GradientText.swift
+//  SwiftUI Advanced
+//
+//  Created by Damien Gautier on 27/03/2023.
+//
+
+import SwiftUI
+
+struct GradientText: View {
+    
+    var text: String = "Text here"
+    
+    var body: some View {
+        Text(text)
+            .gradientForeground(colors: [Color("pink-gradient-1"), Color("pink-gradient-2")])
+    }
+}
+
+extension View {
+    public func gradientForeground(colors: [Color]) -> some View {
+        self
+            .overlay(
+                LinearGradient(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+            )
+            .mask(self)
+    }
+}
